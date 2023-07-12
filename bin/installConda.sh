6d00a31e2641899687249f37b8bc8d342e369380
ckammes@nd.edu <ckammes@nd.edu>
2023-07-11 21:47:23 -0500

    docker-compose

--- bin/installConda.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-curl https://repo.anaconda.com/miniconda/Miniconda3-py39_4.10.3-Linux-x86_64.sh > miniconda.sh
-chmod 755 miniconda.sh
-./miniconda.sh
-
-conda install -c conda-forge tmux vim nodejs nvim
-
-
