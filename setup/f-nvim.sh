e5bf0bc018db02fd2aca7ca630535ab8d396266d
ckammes@nd.edu <ckammes@nd.edu>
2024-01-24 22:50:35 -0800

    When merged, fixes nvim

--- setup/f-nvim.sh
+++ setup/f-nvim.sh
@@ -3,8 +3,14 @@ chmod u+x nvim.appimage
 ./nvim.appimage
 
 if [ $? -eq 0 ]; then
+    sudo mv squashfs-root /
+    sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
+    nvim
     exit 0 # success
 fi
+
+./nvim.appimage --appimage-extract
+./squashfs-root/AppRun --version
 
 sudo mv squashfs-root /
 sudo ln -s /squashfs-root/AppRun /usr/bin/nvim
