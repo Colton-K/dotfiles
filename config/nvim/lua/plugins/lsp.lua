96526e84e3cdbba377723c615d22813df3109064
Colton Kammes <ckammes@amazon.com>
2024-02-13 13:08:43 -0800

    When merged, adds bemol and java support to nvim for AL2

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -41,6 +41,8 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
     vim.lsp.buf.format()
   end, { desc = 'Format current buffer with LSP' })
+
+  bemol()
 end
 
 -- Enable the following language servers
@@ -58,6 +60,7 @@ local servers = {
   dockerls = {},
   html = {},
   yamlls = {},
+  jdtls = {},
 
   lua_ls = {
     Lua = {
@@ -139,3 +142,23 @@ cmp.setup {
     name = 'path',
   },
 }
+
+function bemol()
+ local bemol_dir = vim.fs.find({ '.bemol' }, { upward = true, type = 'directory'})[1]
+ local ws_folders_lsp = {}
+ if bemol_dir then
+  local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+  if file then
+
+   for line in file:lines() do
+    table.insert(ws_folders_lsp, line)
+   end
+   file:close()
+  end
+ end
+
+ for _, line in ipairs(ws_folders_lsp) do
+  vim.lsp.buf.add_workspace_folder(line)
+ end
+
+end
