f43f3980010827d22e3a777159a34ada298d3c37
Colton Kammes <ckammes@amazon.com>
2024-02-16 09:34:37 -0800

    trying to set up neotree

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -132,7 +132,16 @@ return {
           "nvim-lua/plenary.nvim",
           "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
           "MunifTanjim/nui.nvim",
-        }
+        },
+        config = function ()
+            require("neo-tree").setup({
+              follow_current_file = {
+                enabled = true, -- This will find and focus the file in the active buffer every time
+                --               -- the current file is changed while the tree is open.
+                leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+              },
+            })
+        end
     },
 
     {
