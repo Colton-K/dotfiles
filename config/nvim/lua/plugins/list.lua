62bab2951633be5a7026d8432593a5921318d237
Colton Kammes <ckammes@amazon.com>
2024-06-03 14:06:25 -0700

    When merged, adds keychron config and some local help

--- config/nvim/lua/plugins/list.lua
+++ config/nvim/lua/plugins/list.lua
@@ -161,5 +161,11 @@ return {
     {
         "mcchrish/zenbones.nvim",
         requires = "rktjmp/lush.nvim"
+    },
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
     }
 }
