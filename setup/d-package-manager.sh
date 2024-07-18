cc98aef032a839ade5b558d2f51df588cb83ffac
Colton Kammes <ckammes@amazon.com>
2024-07-18 10:23:02 -0700

    fuzzy find projects

--- setup/d-package-manager.sh
+++ setup/d-package-manager.sh
@@ -1,7 +1,15 @@
-package_manager="apt-get install -y "
+package_manager=""
+
 which apt > /dev/null 2>&1
-if test $? -gt 0; then
+if [ $? -eq 0 ]; then
+    package_manager="apt-get install -y "
+elif which dnf > /dev/null 2>&1; then
     package_manager="dnf install -y "
+elif which yum > /dev/null 2>&1; then
+    package_manager="yum install -y "
+else
+    echo "Error: No supported package manager found."
+    exit 1
 fi
 
 echo "Package manager command: $package_manager"
