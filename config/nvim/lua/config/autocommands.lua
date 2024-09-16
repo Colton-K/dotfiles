af40592e1e4b41e7897299c1477945fcaffbedf6
Colton Kammes <ckammes@amazon.com>
2024-09-16 10:52:54 -0700

    changing Z and adding alias

--- config/nvim/lua/config/autocommands.lua
+++ config/nvim/lua/config/autocommands.lua
@@ -37,3 +37,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+vim.api.nvim_create_autocmd({"BufNewFile","BufRead"}, {
+    pattern = "Config",
+    callback = function()
+        local buf = vim.api.nvim_get_current_buf()
+        vim.api.nvim_buf_set_option(buf, "filetype", "perl")
+    end,
+})
